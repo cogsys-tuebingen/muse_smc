b383f9ef0e9c81cf7425437061745e63680eb818
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-12-01 22:52:51 +0100

    multinomial implemenation

--- muse_amcl_core_plugins/src/resampling/multinomial.cpp
+++ muse_amcl_core_plugins/src/resampling/multinomial.cpp
@@ -9,30 +9,46 @@ using namespace muse_amcl;
 
 void Multinomial::apply(ParticleSet &particle_set)
 {
+    /// initalize particle new particle set
     ParticleSet::Particles &p_old = particle_set.getParticles();
-    const std::size_t size = p_old.size();
+    const std::size_t size        = p_old.size();
     ParticleSet::Particles  p_new(size);
 
-    /// Build the cummulative sum
-
-    std::vector<double> cumsum(size);
-    cumsum[0] = p_old.front().weight_;
-    for(std::size_t i = 1 ; i < size; ++i) {
-        cumsum[i] = cumsum[i-1] + p_old[i].weight_;
-    }
-
-    math::random::Uniform<1> rng(cumsum.front(), 1.0);
-    for(std::size_t i = 0 ; i < size ; ++i) {
-        double u = rng.get();
-        double q = 0;
-        for(std::size_t j = 0 ; j < size ; ++j) {
-            if(u > q && u <= cumsum[j]) {
-                p_new[i] = p_old[j];
-                break;
-            }
-            q = cumsum[j];
+    /// prepare ordered sequence of random numbers
+    math::random::Uniform<1> rng(0.0, 1.0);
+    std::vector<double> seq(size, rng.get());
+    {
+        auto seq_it = seq.rbegin();
+        auto seq_it_last = seq_it;
+        auto seq_end = seq.rend();
+        ++seq_it;
+        while(seq_it != seq_end) {
+            *seq_it = rng.get() + *seq_it_last;
+            seq_it_last = seq_it;
+            ++seq_it;
         }
     }
+    /// resampling starts here
+    {
+        double cumsum_last = 0.0;
+        double cumsum = 0.0;
+        auto in_range = [cumsum, cumsum_last] (double u)
+        {
+            return u >= cumsum_last && u < cumsum;
+        };
+        auto seq_it   = seq.begin();
+        auto seq_end  = seq.end();
+        auto p_new_it = p_new.begin();
+        for(const auto &p : p_old) {
+            cumsum += p.weight_;
+            while(seq_it != seq_end && in_range(*seq_it)) {
+                *p_new_it = p;
+            }
+            if(seq_it == seq_end)
+                break;
+        }
+    }
+    /// swap it
     std::swap(p_old, p_new);
 }
 
