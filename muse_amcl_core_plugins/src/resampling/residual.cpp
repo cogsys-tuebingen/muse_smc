6b869b1237f0945f88c90b70a056db901ed8e829
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-12-02 00:33:30 +0100

    build fix

--- muse_amcl_core_plugins/src/resampling/residual.cpp
+++ muse_amcl_core_plugins/src/resampling/residual.cpp
@@ -16,7 +16,7 @@ void Residual::apply(ParticleSet &particle_set)
     auto p_new_it = p_new.begin();
     auto p_new_end = p_new.end();
     for(const auto &p : p_old) {
-        std::size_t copies = std::floor(p._weight_ * size);
+        std::size_t copies = std::floor(p.weight_ * size);
         for(std::size_t i = 0 ; i < copies && p_new_it != p_new_end ; ++i ,++p_new_it) {
             *p_new_it = p;
         }
@@ -25,40 +25,8 @@ void Residual::apply(ParticleSet &particle_set)
     /// if particles to draw are left, do it systematically
     std::size_t left = std::distance(p_new_it, p_new_end);
     if(left > 0) {
-        /// prepare ordered sequence of random numbers
-        std::vector<double> u(left);
-        {
-            math::random::Uniform<1> rng(0.0, 1.0);
-            double u_static = rng.get();
-            for(std::size_t i = 0 ; i < size ; ++i) {
-                u[i] = (i - 1 + u_static) / size;
-            }
-        }
+
     }
-
-
-
-    /// draw samples
-    {
-        double cumsum_last = 0.0;
-        double cumsum = 0.0;
-        auto in_range = [cumsum, cumsum_last] (double u)
-        {
-            return u >= cumsum_last && u < cumsum;
-        };
-        auto u_it   = u.begin();
-        auto u_end  = u.end();
-        auto p_new_it = p_new.begin();
-        for(const auto &p : p_old) {
-            cumsum += p.weight_;
-            while(u_it != u_end && in_range(*u_it)) {
-                *p_new_it = p;
-            }
-            if(u_it == u_end)
-                break;
-        }
-    }
-    /// swap it
     std::swap(p_old, p_new);
 }
 
