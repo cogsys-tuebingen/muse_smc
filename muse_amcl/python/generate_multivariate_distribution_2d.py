a13511b05a20d1c360a9d39309dbfb8030eadb0a
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-01-16 12:08:05 +0100

    added 2 new distribution for testing

--- muse_amcl/python/generate_multivariate_distribution_2d.py
+++ muse_amcl/python/generate_multivariate_distribution_2d.py
@@ -38,35 +38,42 @@ def matrix_to_dictionary(name, arr):
 
     return [name, dictionary]
 
-mean  = [0.0, 0.0, math.pi / 4.0]
+def create_distribution(mu, sigma, n, path):
+    samples = numpy.random.multivariate_normal(mu, sigma, n)
+    est_mu = numpy.mean(samples, axis=0)
+    est_sigma = numpy.cov(samples, rowvar=False)
+    eigen_values, eigen_vectors = numpy.linalg.eig(est_sigma)
+
+    # prepare yaml
+    dump = {}
+    entry_cov = matrix_to_dictionary('covariance', est_sigma)
+    entry_mean = vector_to_dictionary('mean', est_mu)
+    entries_data = []
+    for row in samples:
+        entry = vector_to_dictionary("entry", row)
+        entries_data.append(entry[1])
+    entry_eigen_values = vector_to_dictionary("eigen_values", eigen_values.real)
+    entry_eigen_vectors = matrix_to_dictionary("eigen_vectors", eigen_vectors.real)
+
+    dump[entry_cov[0]] = entry_cov[1]
+    dump[entry_mean[0]] = entry_mean[1]
+    dump["data"] = entries_data
+    dump[entry_eigen_values[0]] = entry_eigen_values[1]
+    dump[entry_eigen_vectors[0]] = entry_eigen_vectors[1]
+
+    stream = open(path, 'w')
+    yaml.dump(dump, stream)
+
+    # plot for visual feedback
+    plot(samples)
+
+
+
+mu = [5.0, 5.0, -math.pi / 4.0]
 sigma = [[0.0025, 0.0, 0.0],
          [0.0, 0.0025, 0.0],
          [0.0, 0.0, 0.1218]]
 
-samples = numpy.random.multivariate_normal(mean, sigma, 500)
-est_mean = numpy.mean(samples, axis=0)
-est_sigma = numpy.cov(samples, rowvar=False)
-eigen_values, eigen_vectors = numpy.linalg.eig(est_sigma)
+create_distribution(mu, sigma, 1000, '/tmp/distribution.yaml')
 
 
-
-dump = {}
-entry_cov  = matrix_to_dictionary('covariance', est_sigma)
-entry_mean = vector_to_dictionary('mean', est_mean)
-entries_data = []
-for row in samples:
-    entry = vector_to_dictionary("entry", row)
-    entries_data.append(entry[1])
-entry_eigen_values = vector_to_dictionary("eigen_values", eigen_values.real)
-entry_eigen_vectors = matrix_to_dictionary("eigen_vectors", eigen_vectors.real)
-
-dump[entry_cov[0]] = entry_cov[1]
-dump[entry_mean[0]] = entry_mean[1]
-dump["data"] = entries_data
-dump[entry_eigen_values[0]] = entry_eigen_values[1]
-dump[entry_eigen_vectors[0]] = entry_eigen_vectors[1]
-
-stream = open('/tmp/distribution.yaml', 'w')
-yaml.dump(dump, stream)
-
-plot(samples)
