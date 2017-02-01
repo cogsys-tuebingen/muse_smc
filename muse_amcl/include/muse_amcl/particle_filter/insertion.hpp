7a1bb7c74d02b5b9b15ddc35469d096c0b595b6f
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-02-01 12:01:13 +0100

    documentation

--- muse_amcl/include/muse_amcl/particle_filter/insertion.hpp
+++ muse_amcl/include/muse_amcl/particle_filter/insertion.hpp
@@ -6,66 +6,105 @@
 #include <muse_amcl/utils/delegate.hpp>
 
 namespace muse_amcl {
+/**
+ * @brief The Insertion class is used to fill up a particle set.
+ *        The insertion object notifies usage and changes.
+ */
 class Insertion {
 public:
     using notify_finished = delegate<void()>;
     using notify_update   = delegate<void(const Particle &)>;
 
+    /**
+     * @brief Insertion constructor.
+     * @param data      - data structure to insert to
+     * @param update    - on change notification callback
+     * @param finshed   - on finish callback
+     */
     Insertion(std::buffered_vector<Particle> &data,
               notify_update                   update,
               notify_finished                 finshed) :
         data_(data),
         open_(true),
         update_(update),
-        finished_(finshed)
+        closed_(finshed)
     {
     }
 
+    /**
+     * @brief Insertion destructor.
+     */
     virtual ~Insertion()
     {
         if(open_) {
-            finished_();
+            closed_();
         }
     }
 
+    /**
+     * @brief Insert by move.
+     * @param sample - sample to be inserted
+     */
     inline void insert(Particle&& sample)
     {
+        if(!open_)
+            return;
+
         data_.emplace_back(std::move(sample));
         update_(sample);
     }
 
+    /**
+     * @brief Insert by reference.
+     * @param sample - sample to be inserted
+     */
     inline void insert(const Particle &sample)
     {
+        if(!open_)
+            return;
+
         data_.push_back(sample);
         update_(sample);
     }
 
+    /**
+     * @brief Check wether insertion is still possible.
+     * @return insertion is possible
+     */
     inline bool canInsert() const
     {
-        return data_.size() < data_.capacity();
+        return data_.size() < data_.capacity() && open_;
     }
 
+    /**
+     * @brief Close an insertion. After that it cannot be used
+     *        anymore to fill up a vector.
+     */
     inline void close()
     {
         if(open_) {
-            finished_();
+            closed_();
             open_ = false;
         } else {
             throw std::runtime_error("Insertion cannot be closed twice!");
         }
     }
 
-    std::buffered_vector<Particle> const & set()
+    /**
+     * @brief Return const reference to container the data is inserted to.
+     * @return const reference to the data container
+     */
+    std::buffered_vector<Particle> const & getData()
     {
         return data_;
     }
 
 private:
-    std::buffered_vector<Particle> &data_;
+    std::buffered_vector<Particle> &data_;        /// the data container
 
-    bool                            open_;
-    notify_update                   update_;
-    notify_finished                 finished_;
+    bool                            open_;        /// indicator if insertion is still open
+    notify_update                   update_;      /// on update callback
+    notify_finished                 closed_;      /// on close / finish callback
 
 };
 }
--- muse_amcl/include/muse_amcl/particle_filter/member_iterator.hpp
+++ muse_amcl/include/muse_amcl/particle_filter/member_iterator.hpp
@@ -5,6 +5,11 @@
 #include <muse_amcl/utils/delegate.hpp>
 
 namespace muse_amcl {
+/**
+ * @brief The MemberIterator class is used to make a certain member of a data
+ *        class available, whereas the full data entry can only be accessed by
+ *        const reference.
+ */
 template<typename Data, typename T, T Data::*Member>
 class MemberIterator : public std::iterator<std::random_access_iterator_tag, T>
 {
@@ -14,10 +19,15 @@ class MemberIterator : public std::iterator<std::random_access_iterator_tag, T>
 
     using notify_update = delegate<void(const T &)>;
 
-    Data            *data_;
-    notify_update    update_;
+    Data            *data_;     /// data container content
+    notify_update    update_;   /// on update callback
 
 public:
+    /**
+     * @brief MemberIterator constructor.
+     * @param begin     - point to the data in memory
+     * @param update    - on update callback
+     */
     explicit MemberIterator(Data         *begin,
                             notify_update update) :
         data_(begin),
@@ -25,6 +35,13 @@ public:
     {
     }
 
+    /**
+     * @brief operator ++ increments the position within the iterable data and executes the
+     *        update callback for the previously accessed data.
+     *        It is assumed that the data is iterated till the (n+1)th element though the
+     *        n-th element is the last one meant to be accessed.
+     * @return the new iterator state
+     */
     inline iterator& operator++()
     {
         update_(data_->*Member);
@@ -32,34 +49,62 @@ public:
         return *this;
     }
 
+    /**
+     * @brief operator == compares two member iterators.
+     * @param _other    - another member iterator
+     * @return whether bother itaterators are equal or not
+     */
     inline bool operator ==(const MemberIterator<Data, T, Member> &_other) const
     {
         return data_ == _other.data_;
     }
 
+    /**
+     * @brief operator != check for inequality of two iterators, meaning that they currently
+     *        point to two different positions within the data structure.
+     * @param _other - another data iterator
+     * @return
+     */
     inline bool operator !=(const MemberIterator<Data, T, Member> &_other) const
     {
         return !(*this == _other);
     }
 
+    /**
+     * @brief operator * dereferences the member to be accessible.
+     * @return reference to the member
+     */
     inline reference operator *() const
     {
         return (data_->*Member);
     }
 
+    /**
+     * @brief getData returns a const reference to the full data entry not allowing
+     *        to change anything. Read only access is the result of that.
+     * @return
+     */
     inline const Data& getData() const
     {
         return *data_;
     }
-
 };
 
+/**
+ * @brief The MemberDecorator class is used to retrieve member iterators for begin and
+ *        end of the data structure, as well as for the type setup.
+ */
 template<typename Data, typename T, T Data::*Member>
 class MemberDecorator {
 public:
     using iterator = MemberIterator<Data, T, Member>;
     using notify_update = delegate<void(const T&)>;
 
+    /**
+     * @brief MemberDecorator constrcutor.
+     * @param data   - the data structure which shall be iterated
+     * @param update - on update callback
+     */
     MemberDecorator(std::buffered_vector<Data> &data,
                     notify_update                update) :
         data_(data),
@@ -67,22 +112,28 @@ public:
     {
     }
 
+    /**
+     * @brief  begin returns and iterator pointing to the begin of the data structure
+     *         to be iterated.
+     * @return iterator at position zero of data
+     */
     inline iterator begin()
     {
         return iterator(&data_.front(), update_);
     }
 
+    /**
+     * @brief end returns an iterator pointing to the end of the data structure being iterated.
+     *        As the std iterator, the end iterator points to the position n+1 if the content
+     *        of data is of size n.
+     * @return iterato at position n+1
+     */
     inline iterator end() {
-        return iterator(&data_.back() + 1, update_);   /// end has always to be set off by 1
+        return iterator(&data_.back() + 1, update_);
     }
 private:
-    std::buffered_vector<Data> &data_;
-    notify_update               update_;
+    std::buffered_vector<Data> &data_;      /// the container to be iterated
+    notify_update               update_;    /// on update callback
 };
-
-
 }
-
-
-
 #endif // PARTICLE_SET_MEMBER_ITERATOR_HPP
