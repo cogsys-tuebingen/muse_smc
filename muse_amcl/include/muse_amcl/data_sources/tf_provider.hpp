4aabe1709aa95e534f2a28c015422116a7efde62
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2017-02-05 13:45:10 +0100

    locked tf provider

--- muse_amcl/include/muse_amcl/data_sources/tf_provider.hpp
+++ muse_amcl/include/muse_amcl/data_sources/tf_provider.hpp
@@ -3,11 +3,92 @@
 
 #include <tf/transform_listener.h>
 #include <memory>
+#include <muse_amcl/utils/delegate.hpp>
 #include <mutex>
 
 namespace muse_amcl {
 class TFProvider {
 public:
+    class LockedTFProvider {
+    public:
+        LockedTFProvider(std::unique_lock<std::mutex> &&lock,
+                         TFProvider &provider) :
+            lock_(std::move(lock)),
+            provider_(provider)
+        {
+        }
+
+        LockedTFProvider(LockedTFProvider &&other) :
+            lock_(std::move(other.lock_)),
+            provider_(other.provider_)
+        {
+        }
+
+        virtual ~LockedTFProvider()
+        {
+            lock_.unlock();
+        }
+
+        inline bool lookupTransform(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::StampedTransform &transform)
+        {
+            return provider_.lookupTransformImpl(target_frame, source_frame, time, transform);
+        }
+
+        inline bool lookupTransform(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::StampedTransform &transform,
+                                    const ros::Duration  &timeout)
+        {
+            return provider_.lookupTransformImpl(target_frame, source_frame, time, transform, timeout);
+        }
+
+
+        inline bool lookupTransform(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::Transform        &transform)
+        {
+            return provider_.lookupTransform(target_frame, source_frame, time, transform);
+        }
+
+        inline bool lookupTransform(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::Transform        &transform,
+                                    const ros::Duration  &timeout)
+        {
+            return provider_.lookupTransformImpl(target_frame, source_frame, time, transform, timeout);
+        }
+
+        inline bool canTransform(const std::string &target_frame,
+                                 const std::string &source_frame,
+                                 const ros::Time   &time)
+        {
+            return provider_.tf_.canTransform(target_frame, source_frame, time);
+        }
+
+        inline bool waitForTransform(const std::string &target_frame,
+                                     const std::string &source_frame,
+                                     const ros::Time &time,
+                                     const ros::Duration &timeout)
+        {
+            return provider_.tf_.waitForTransform(target_frame, source_frame, time, timeout);
+        }
+
+        inline void getFrameStrings(std::vector<std::string> &frames)
+        {
+            provider_.tf_.getFrameStrings(frames);
+        }
+    protected:
+        std::unique_lock<std::mutex>  lock_;
+        TFProvider                   &provider_;
+    };
+
+    friend class LockedTFProvider;
     typedef std::shared_ptr<TFProvider> Ptr;
 
     TFProvider()
@@ -20,13 +101,7 @@ public:
                                 tf::StampedTransform &transform)
     {
         std::unique_lock<std::mutex> l(mutex_);
-        try  {
-            tf_.lookupTransform(target_frame, source_frame, time, transform);
-        } catch (const tf::TransformException &e) {
-            std::cerr << e.what() << std::endl;
-            return false;
-        }
-        return true;
+        return lookupTransformImpl(target_frame, source_frame, time, transform);
     }
 
     inline bool lookupTransform(const std::string    &target_frame,
@@ -36,11 +111,7 @@ public:
                                 const ros::Duration  &timeout)
     {
         std::unique_lock<std::mutex> l(mutex_);
-        if(tf_.waitForTransform(target_frame, source_frame, time, timeout)) {
-            tf_.lookupTransform(target_frame, source_frame, time, transform);
-            return true;
-        }
-        return false;
+        return lookupTransformImpl(target_frame, source_frame, time, transform, timeout);
     }
 
 
@@ -50,15 +121,7 @@ public:
                                 tf::Transform        &transform)
     {
         std::unique_lock<std::mutex> l(mutex_);
-        tf::StampedTransform stamped;
-        try  {
-            tf_.lookupTransform(target_frame, source_frame, time, stamped);
-            transform = stamped;
-        } catch (const tf::TransformException &e) {
-            std::cerr << e.what() << std::endl;
-            return false;
-        }
-        return true;
+        return lookupTransform(target_frame, source_frame, time, transform);
     }
 
     inline bool lookupTransform(const std::string    &target_frame,
@@ -68,16 +131,7 @@ public:
                                 const ros::Duration  &timeout)
     {
         std::unique_lock<std::mutex> l(mutex_);
-        tf::StampedTransform stamped;
-        std::string error;
-        if(tf_.waitForTransform(target_frame, source_frame, time, timeout, ros::Duration(0.01), &error)) {
-            tf_.lookupTransform(target_frame, source_frame, time, stamped);
-            transform = stamped;
-            return true;
-        } else {
-            std::cerr << error << std::endl;
-        }
-        return false;
+        return lookupTransformImpl(target_frame, source_frame, time, transform, timeout);
     }
 
     inline bool canTransform(const std::string &target_frame,
@@ -103,10 +157,82 @@ public:
         tf_.getFrameStrings(frames);
     }
 
+    inline void getLockedTFProvider()
+    {
 
-private:
+    }
+
+
+protected:
     std::mutex mutex_;
     tf::TransformListener tf_;
+
+    inline bool lookupTransformImpl(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::StampedTransform &transform)
+    {
+        try  {
+            tf_.lookupTransform(target_frame, source_frame, time, transform);
+        } catch (const tf::TransformException &e) {
+            std::cerr << e.what() << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    inline bool lookupTransformImpl(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::StampedTransform &transform,
+                                    const ros::Duration  &timeout)
+    {
+        if(tf_.waitForTransform(target_frame, source_frame, time, timeout)) {
+            tf_.lookupTransform(target_frame, source_frame, time, transform);
+            return true;
+        }
+        return false;
+    }
+
+
+    inline bool lookupTransformImpl(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::Transform        &transform)
+    {
+        tf::StampedTransform stamped;
+        try  {
+            tf_.lookupTransform(target_frame, source_frame, time, stamped);
+            transform = stamped;
+        } catch (const tf::TransformException &e) {
+            std::cerr << e.what() << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    inline bool lookupTransformImpl(const std::string    &target_frame,
+                                    const std::string    &source_frame,
+                                    const ros::Time      &time,
+                                    tf::Transform        &transform,
+                                    const ros::Duration  &timeout)
+    {
+        tf::StampedTransform stamped;
+        std::string error;
+        if(tf_.waitForTransform(target_frame, source_frame, time, timeout, ros::Duration(0.01), &error)) {
+            tf_.lookupTransform(target_frame, source_frame, time, stamped);
+            transform = stamped;
+            return true;
+        } else {
+            std::cerr << error << std::endl;
+        }
+        return false;
+    }
+
+
+
+
+
 };
 }
 #endif // TF_PROVIDER_HPP
