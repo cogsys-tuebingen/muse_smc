5c70c143ac6468064d4ffc9aa113259f8be7d215
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2017-05-15 20:11:57 +0200

    adaptive beam model with mle estimation for parameters

---
+++ muse_amcl_core_plugins/src/laser_2d/beam_model_mle.h
@@ -0,0 +1,34 @@
+#ifndef BEAM_MODEL_MLE_H
+#define BEAM_MODEL_MLE_H
+
+#include <muse_amcl/particle_filter/update.hpp>
+
+namespace muse_amcl {
+class BeamModelMLE : public UpdateModel
+{
+public:
+    BeamModelMLE();
+
+    virtual void update(const Data::ConstPtr &data,
+                        const Map::ConstPtr &map,
+                        ParticleSet::Weights set) override;
+
+protected:
+    std::size_t max_beams_;
+    double      z_hit_;
+    double      z_short_;
+    double      z_max_;
+    double      z_rand_;
+    double      sigma_hit_;
+    double      denominator_hit_;
+    double      lambda_short_;
+    double      chi_outlier_;
+
+    virtual void doSetup(ros::NodeHandle &nh_private) override;
+
+    //// DEBUG
+    ros::Publisher pub_debug_;
+
+};
+}
+#endif // BEAM_MODEL_MLE_H
