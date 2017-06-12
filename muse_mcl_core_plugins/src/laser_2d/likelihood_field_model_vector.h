507a91a3a98a8e2d4693928cab29025d00eb6f12
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2017-06-12 13:44:52 +0200

    setting up line-segment based sensor models

---
+++ muse_mcl_core_plugins/src/laser_2d/likelihood_field_model_vector.h
@@ -0,0 +1,11 @@
+#ifndef LIKELIHOOD_FIELD_MODEL_VECTOR_H
+#define LIKELIHOOD_FIELD_MODEL_VECTOR_H
+
+
+class LikelihoodFieldModelVector
+{
+public:
+    LikelihoodFieldModelVector();
+};
+
+#endif // LIKELIHOOD_FIELD_MODEL_VECTOR_H
