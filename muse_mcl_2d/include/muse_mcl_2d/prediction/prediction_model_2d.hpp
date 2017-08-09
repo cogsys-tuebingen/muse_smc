d4715a47e8c1b1b7575d35e777d1b6892934973a
richard hanten <richard.hanten@uni-tuebingen.de>
2017-08-09 23:37:07 +0200

    current state of port

---
+++ muse_mcl_2d/include/muse_mcl_2d/prediction/prediction_model_2d.hpp
@@ -0,0 +1,17 @@
+#ifndef PREDICTION_MODEL_2D_HPP
+#define PREDICTION_MODEL_2D_HPP
+
+#include <muse_smc/prediction/prediction_model.hpp>
+#include <muse_mcl_2d/samples/sample_2d.hpp>
+
+namespace muse_mcl_2d {
+class PredictionModel2D : public muse_smc::PredictionModel<Sample2D>
+{
+public:
+
+protected:
+
+};
+}
+
+#endif // PREDICTION_MODEL_2D_HPP
