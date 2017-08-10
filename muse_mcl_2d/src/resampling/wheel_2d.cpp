c0d0ce3500439788c505e5604e4e098f0b3ce0db
richard hanten <richard.hanten@uni-tuebingen.de>
2017-08-11 00:25:24 +0200

    done

---
+++ muse_mcl_2d/src/resampling/wheel_2d.cpp
@@ -0,0 +1,24 @@
+#include <muse_smc/resampling/wheel.hpp>
+#include <muse_mcl_2d/samples/sample_2d.hpp>
+
+#include <ros/ros.h>
+
+#include <class_loader/class_loader_register_macro.h>
+
+namespace muse_mcl_2d {
+using Resampling = muse_smc::Resampling<muse_mcl_2d::Sample2D>;
+class WheelOfFortune : public muse_smc::WheelOfFortune<muse_mcl_2d::Sample2D>
+{
+public:
+    void setup(const typename sample_uniform_t::Ptr &uniform_pose_sampler,
+               ros::NodeHandle &nh)
+    {
+        auto param_name = [this](const std::string &name){return name_ + "/" + name;};
+        muse_smc::WheelOfFortune<muse_mcl_2d::Sample2D>::setup(uniform_pose_sampler,
+                                                               nh.param(param_name("recovery_alpha_fast"), 0.0),
+                                                               nh.param(param_name("recovery_alpha_slow"), 0.0));
+    }
+};
+}
+
+CLASS_LOADER_REGISTER_CLASS(muse_mcl_2d::WheelOfFortune, muse_mcl_2d::Resampling)
