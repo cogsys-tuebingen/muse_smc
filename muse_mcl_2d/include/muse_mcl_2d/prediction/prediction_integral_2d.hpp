6ee718e3ff0bc248d2d98fa08a1153abdfb46c24
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-08-10 18:29:27 +0200

    completing 2d version

--- muse_mcl_2d/include/muse_mcl_2d/prediction/prediction_integral_2d.hpp
+++ muse_mcl_2d/include/muse_mcl_2d/prediction/prediction_integral_2d.hpp
@@ -1,4 +1,18 @@
 #ifndef PREDICTION_INTEGRAL_2D_HPP
 #define PREDICTION_INTEGRAL_2D_HPP
 
+#include <muse_mcl_2d/samples/sample_2d.hpp>
+#include <muse_smc/prediction/prediction_integral.hpp>
+
+namespace muse_mcl_2d {
+class PredictionIntegral2D : public muse_smc::PredictionIntegral<Sample2D>
+{
+public:
+
+protected:
+};
+
+}
+
+
 #endif // PREDICTION_INTEGRAL_2D_HPP
