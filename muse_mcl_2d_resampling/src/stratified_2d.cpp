5d757a25cc5556d793cf500568f962058afa8f2d
richard hanten <richard.hanten@uni-tuebingen.de>
2017-08-24 21:06:08 +0200

    removed packages became unnecessary

--- muse_mcl_2d_resampling/src/stratified_2d.cpp
+++
@@ -1,4 +0,0 @@
-#include <muse_mcl/resampling/stratified.hpp>
-
-#include <class_loader/class_loader_register_macro.h>
-CLASS_LOADER_REGISTER_CLASS(muse_mcl::Stratified, muse_mcl::Resampling)
--- muse_mcl_2d_resampling/src/wheel_2d.cpp
+++
@@ -1,4 +0,0 @@
-#include <muse_mcl/resampling/wheel.hpp>
-
-#include <class_loader/class_loader_register_macro.h>
-CLASS_LOADER_REGISTER_CLASS(muse_mcl::WheelOfFortune, muse_mcl::Resampling)
