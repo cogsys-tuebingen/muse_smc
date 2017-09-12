ce2381a2adc51ecaeb5a7d17295f4117d3828089
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-09-12 20:45:13 +0200

    chunk

--- muse_mcl_2d_gridmaps/include/muse_mcl_2d_gridmaps/dynamic_maps/chunk.hpp
+++ muse_mcl_2d_gridmaps/include/muse_mcl_2d_gridmaps/dynamic_maps/chunk.hpp
@@ -1,12 +1,35 @@
 #ifndef CHUNK_HPP
 #define CHUNK_HPP
 
+#include <vector>
+
 namespace muse_mcl_2d_gridmaps {
 namespace dynamic_maps {
 template<typename T>
 class Chunk {
 public:
-    Chunk() = default;
+    Chunk(const std::size_t size,
+          const T default_value) :
+        size_(size),
+        data_(size_ * size_, default_value)
+    {
+    }
+
+    inline T const & at(const int x, const int y) const
+    {
+        data_ptr_[y * size + x];
+    }
+
+    inline T & at (const int x, const int y)
+    {
+        data_ptr[y * size + x];
+    }
+
+private:
+    const std::size_t   size_;
+    std::vector<T>      data_;
+    T                  *data_ptr_;
+
 };
 }
 }
