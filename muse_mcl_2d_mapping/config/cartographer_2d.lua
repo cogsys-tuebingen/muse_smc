08b91297bd0041d38c75b3a2cf337d877abe193f
richard hanten <richard.hanten@uni-tuebingen.de>
2017-12-05 03:49:17 +0100

    cartograher correction

--- muse_mcl_2d_mapping/config/cartographer_2d.lua
+++ muse_mcl_2d_mapping/config/cartographer_2d.lua
@@ -23,19 +23,20 @@ options = {
   imu_sampling_ratio = 1.
 }
 
+
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.)
 TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.hit_probability = 0.55
 TRAJECTORY_BUILDER_2D.submaps.range_data_inserter.miss_probability = 0.45
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
+
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-MAP_BUILDER.num_background_threads = 2
-SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 5e2
-SPARSE_POSE_GRAPH.optimize_every_n_scans = 10
-SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-SPARSE_POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.62
+MAP_BUILDER.num_background_threads = 8
+
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
 return options
