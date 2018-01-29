e36778d97e21d43e22b1bf90319a522635f51da7
richard hanten <richard.hanten@uni-tuebingen.de>
2018-01-29 23:05:40 +0100

    restructuring part I

--- muse_mcl_2d/include/muse_mcl_2d/samples/sample_density_2d.hpp
+++ muse_mcl_2d/include/muse_mcl_2d/samples/sample_density_2d.hpp
@@ -7,14 +7,10 @@
 
 #include <muse_smc/samples/sample_density.hpp>
 
-#include <muse_mcl_2d/samples/sample_indexation_2d.hpp>
-#include <muse_mcl_2d/samples/sample_density_data_2d.hpp>
-#include <muse_mcl_2d/samples/sample_clustering_2d.hpp>
+#include <muse_mcl_2d/samples/sample_2d.hpp>
 
-#include <cslibs_indexed_storage/storage.hpp>
-#include <cslibs_indexed_storage/backend/kdtree/kdtree_buffered.hpp>
-
-namespace cis = cslibs_indexed_storage;
+#include <cslibs_math/statistics/weighted_angular_mean.hpp>
+#include <cslibs_math/statistics/weighted_distribution.hpp>
 
 namespace muse_mcl_2d {
 class SampleDensity2D : public muse_smc::SampleDensity<Sample2D>
@@ -28,10 +24,17 @@ public:
         return "muse_mcl_2d::SampleDensity2D";
     }
 
-    using clustering_t              = SampleClustering2D;
-    using cluster_map_t             = clustering_t::cluster_map_t;
-    using distribution_map_t        = clustering_t::distribution_map_t;
-    using angular_mean_map_t        = clustering_t::angular_mean_map_t;
+    using distribution_t      = cslibs_math::statistics::WeightedDistribution<2>;
+    using angular_mean_t      = cslibs_math::statistics::WeightedAngularMean;
+    using allocator_t         = Eigen::aligned_allocator<std::pair<const int, distribution_t>>;
+    using distribution_map_t  = std::unordered_map<int,
+                                                  distribution_t,
+                                                  std::hash<int>,
+                                                  std::equal_to<int>,
+                                                  allocator_t>;
+    using angular_mean_map_t = std::unordered_map<int, angular_mean_t>;
+    using sample_ptr_vector_t = std::vector<const Sample2D *>;
+    using cluster_map_t = std::unordered_map<int, sample_ptr_vector_t>;
 
     virtual void setup(ros::NodeHandle &nh) = 0;
     virtual cluster_map_t const & clusters() const = 0;
