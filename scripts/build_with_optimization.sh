494802ced4adb503b630d909bfebb1454f1f61af
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-05-15 18:33:09 +0200

    added just another useful cmake flag

--- scripts/build_with_optimization.sh
+++ scripts/build_with_optimization.sh
@@ -3,4 +3,4 @@ if [ "$1" = "--clean" ]
 	then
 		catkin_make clean
 fi
-catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Ofast 
+catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Ofast -DCMAKE_LINKER=/usr/bin/ld.gol
