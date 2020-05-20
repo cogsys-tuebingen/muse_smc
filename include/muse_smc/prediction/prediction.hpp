908f47efdcfd2d58b6d5d193ec9549582114c54d
Richard Hanten <richard.hanten@gmail.com>
2020-05-20 19:02:20 +0200

    whatever

--- include/muse_smc/prediction/prediction.hpp
+++ include/muse_smc/prediction/prediction.hpp
@@ -1,9 +1,7 @@
 #ifndef PREDICTION_HPP
 #define PREDICTION_HPP
 
-#include <cslibs_time/time_frame.hpp>
-#include <muse_smc/samples/sample_set.hpp>
-#include <muse_smc/state_space/state_space.hpp>
+#include <cslibs_time/time.hpp>
 
 namespace muse_smc {
 template <typename prediction_model_t>
@@ -41,46 +39,39 @@ class Prediction {
     }
   };
 
-  Prediction(const typename data_t::ConstPtr &data,
-             const typename prediction_model_t::Ptr &model)
-      : data_(data), model_(model) {}
+  inline explicit Prediction(const typename data_t::ConstPtr &data,
+                             const typename prediction_model_t::Ptr &model)
+      : data_{data}, model_{model} {}
 
-  Prediction(const typename data_t::ConstPtr &data,
-             const typename state_space_t::ConstPtr &state_space,
-             const typename prediction_model_t::Ptr &model)
-      : data_(data), state_space_(state_space), model_(model) {}
+  inline explicit Prediction(
+      const typename data_t::ConstPtr &data,
+      const typename state_space_t::ConstPtr &state_space,
+      const typename prediction_model_t::Ptr &model)
+      : data_{data}, state_space_{state_space}, model_{model} {}
 
   virtual ~Prediction() = default;
 
-  inline typename prediction_model_t::Result operator()(
-      const cslibs_time::Time &until,
-      typename sample_set_t::state_iterator_t states) {
+  inline auto operator()(const cslibs_time::Time &until,
+                         typename sample_set_t::state_iterator_t states) {
     return state_space_ ? model_->apply(data_, state_space_, until, states)
                         : model_->apply(data_, until, states);
   }
 
-  inline typename prediction_model_t::Result::Ptr apply(
-      const cslibs_time::Time &until,
-      typename sample_set_t::state_iterator_t states) {
+  inline auto apply(const cslibs_time::Time &until,
+                    typename sample_set_t::state_iterator_t states) {
     return state_space_ ? model_->apply(data_, state_space_, until, states)
                         : model_->apply(data_, until, states);
   }
 
-  inline auto const &getStamp() const {
-    return data_->timeFrame().end;
-  }
+  inline auto const &getStamp() const { return data_->timeFrame().end; }
 
-  inline const cslibs_time::TimeFrame &timeFrame() const {
-    return data_->timeFrame();
-  }
+  inline auto const &timeFrame() const { return data_->timeFrame(); }
 
-  inline cslibs_time::Time const &stampReceived() const {
-    return data_->stampReceived();
-  }
+  inline auto const &stampReceived() const { return data_->stampReceived(); }
 
-  inline const std::string &getModelName() const { return model_->getName(); }
+  inline auto const &getModelName() const { return model_->getName(); }
 
-  inline typename prediction_model_t::Ptr getModel() const { return model_; }
+  inline auto getModel() const { return model_; }
 
  private:
   typename data_t::ConstPtr data_;
--- include/muse_smc/prediction/prediction_relay.hpp
+++ include/muse_smc/prediction/prediction_relay.hpp
@@ -1,66 +1,53 @@
 #ifndef PREDICTION_RELAY_HPP
 #define PREDICTION_RELAY_HPP
 
-#include <muse_smc/state_space/state_space_provider.hpp>
+#include <muse_smc/prediction/prediction.hpp>
 #include <muse_smc/smc/smc.hpp>
 
 namespace muse_smc {
-template<typename smc_t>
-class PredictionRelay
-{
-public:
-    using Ptr                     = std::shared_ptr<PredictionRelay>;
-    using types_t                 = typename smc_t::types_t;
-    using sample_t                = typename types_t::sample_t;
-    using prediction_t            = typename types_t::prediction_t;
-    using prediction_model_t      = typename types_t::prediction_model_t;
-    using state_space_provider_t  = typename types_t::state_space_provider_t;
-    using state_space_t           = typename types_t::state_space_t;
-    using data_provider_t         = typename muse_smc::traits::DataProvider<sample_t>::type;
-    using data_t                  = typename muse_smc::traits::Data<sample_t>::type;
+template <typename smc_t, typename prediction_model_t, typename data_provider_t, typename state_space_provider_t>
+class PredictionRelay {
+ public:
+  using Ptr = std::shared_ptr<PredictionRelay>;
+  using prediction_t = Prediction<prediction_model_t>;
+  using data_t = typename prediction_model_t::data_t;
 
+  inline explicit PredictionRelay(const typename smc_t::Ptr &smc) : smc_{smc} {}
 
-    PredictionRelay(const typename smc_t::Ptr &smc) :
-        smc_(smc)
-    {
-    }
+  virtual ~PredictionRelay() = default;
 
-    virtual ~PredictionRelay() = default;
+  inline void relay(const typename prediction_model_t::Ptr &p,
+                    const typename data_provider_t::Ptr &d) {
+    auto callback = [this, p](const typename data_t::ConstPtr &data) {
+      typename prediction_t::Ptr prediction(new prediction_t(data, p));
+      smc_->addPrediction(prediction);
+    };
+    handle_ = d->connect(callback);
+  }
 
-    inline void relay(const typename prediction_model_t::Ptr &p,
-                      const typename data_provider_t::Ptr &d)
-    {
-        auto callback = [this, p](const typename data_t::ConstPtr &data)
-        {
-            typename prediction_t::Ptr prediction(new prediction_t(data, p));
-            smc_->addPrediction(prediction);
-        };
+  inline void relay(const typename prediction_model_t::Ptr &p,
+                    const typename data_provider_t::Ptr &d,
+                    const typename state_space_provider_t::Ptr &s) {
+    auto callback = [this, p, s](const typename data_t::ConstPtr &data) {
+      const auto ss = s->getStateSpace();
+      if (ss) {
+        typename prediction_t::Ptr prediction(new prediction_t(data, ss, p));
+        smc_->addPrediction(prediction);
+      } else {
+        std::cerr << "[PredictionRelay]: " << s->getName()
+                  << " supplied state space which was zero!"
+                  << "\n";
+        std::cerr << "[PredictionRelay]: Dropped prediction!"
+                  << "\n";
+      }
+    };
+    handle_ = d->connect(callback);
+  }
 
-        handle_ = d->connect(callback);
-    }
-
-    inline void relay(const typename prediction_model_t::Ptr &p,
-                      const typename data_provider_t::Ptr &d,
-                      const typename state_space_provider_t::Ptr &s)
-    {
-        auto callback = [this, p, s](const typename data_t::ConstPtr &data)
-        {
-            typename state_space_t::ConstPtr ss = s->getStateSpace();
-            if (ss) {
-                typename prediction_t::Ptr prediction(new prediction_t(data, ss, p));
-                smc_->addPrediction(prediction);
-            } else {
-                std::cerr << "[PredictionRelay]: " << s->getName() << " supplied state space which was zero!" << "\n";
-                std::cerr << "[PredictionRelay]: Dropped prediction!" << "\n";
-            }
-        };
-        handle_ = d->connect(callback);
-    }
-
-private:
-    typename smc_t::Ptr                         smc_;
-    typename data_provider_t::connection_t::Ptr handle_;
+ private:
+  typename smc_t::Ptr smc_;
+  typename data_provider_t::connection_t::Ptr handle_;
 };
-}
+}  // namespace muse_smc
 
-#endif // PREDICTION_RELAY_HPP
+#endif  // PREDICTION_RELAY_HPP
